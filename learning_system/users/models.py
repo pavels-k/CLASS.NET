eef37732cbe2ce0d044f07d8c7fc5b9ddfc6c442
pavels-k <pavelrusk@gmail.com>
2020-01-24 00:46:27 +0300

    Update tests

--- learning_system/users/models.py
+++ learning_system/users/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils import timezone
 from learning_system.practice.models import PracticeTask
-from django.core.exceptions import NON_FIELD_ERRORS, ValidationError
+from django.core.exceptions import ValidationError
     
 class User(AbstractUser):
     is_staff = models.BooleanField(default=False)
@@ -21,12 +21,10 @@ class Group(models.Model):
     user = models.ManyToManyField(User, verbose_name='Пользователь')
     name = models.CharField(max_length=50, verbose_name='Название курса')
     date_of_creation = models.DateTimeField(default=timezone.now, verbose_name='Дата создания')
-    student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name='Student_course')
-    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE, related_name='Teacher_course')
     available_subjects = models.ManyToManyField("self", verbose_name='Доступные предметы')
     
 def  group_validate(self):
-    if (self.role == Group.student) and (self.groups.all().count() > 0):
+    if (self.role == Group.user) and (self.groups.all().count() > 0):
         raise ValidationError("Студент может быть только в одной группе")
     try:
         self.User.full_clean()
--- tests/test_models.py
+++ tests/test_models.py
@@ -1,23 +1,57 @@
 from django.test import TestCase
-from learning_system.users.models import User, Student, Group, group_validate
+from learning_system.users.models import User, Student, Group, Teacher
+from learning_system.users.models import User, Student, Group, Teacher
+from learning_system.courses.models import Course
+from learning_system.theory.models import TheoryPost, TheoryCategory
+from learning_system.practice.models import PracticeType, PracticeCategory, PracticeTask, PracticeTaskVariation
 
 class UserModelTest(TestCase):
 
     @classmethod
-    def setUp(self):
-        self.id1 = User.objects.create(username='Иван')
-        self.id2 = User.objects.create(username='Ivan')
-        self.st1 = Student.objects.create(username = 'din')
-        self.st2 = Student.objects.create(username = 'greg')
-        
-        Group.objects.create(student_id = 1, name = '404')
+    def setUp(cls):
+        pass
+
+    def test_1(self):
+        course_1 = Course.objects.create(title = 'Математический анализ')
+        course_2 = Course.objects.create(title = 'ТВиМС')
+
+    def test_2(self):
+        course_1 = Course.objects.create(title = 'Математический анализ')
+        #course_2 = Course.objects.create(title = 'ТВиМС')
+        theorypost_1 = TheoryPost.objects.create(content = 'Текст теоретического материала', course = course_1)
+        practicecategory_1 = PracticeCategory.objects.create(course = course_1)
+        practicetask_1 = PracticeTask.objects.create(content = 'Текст Практического материала материала', practicecategory = practicecategory_1)
+        self.assertEqual(theorypost_1.course == course_1, True)
+        self.assertEqual(practicecategory_1.course == course_1, True)
 
 
-    def test_publish_method_for_post(self):
-        gruppa = Group.objects.get(title='Blog Post #1')
-        #self.assertEqual(, True)
+'''
 
-    def test_one_plus_one_equals_two(self):
-        print("Method: test_one_plus_one_equals_two.")
-        self.assertEqual(1+1, 2)
+    def test_6(self):
+        student = Student.objects.create(username = 'Student #1')
+        teacher = Teacher.objects.create(username = 'Teacher #1')
+        print(Student.objects.all())
+        self.assertEqual(Student.objects.all().count() > 1, False)
+    
+    def test_9(self):
+        pass
+
+
+    def test_7(self):
+        teacher = Teacher.objects.create(username = 'Din')
+        group_1 = Group.objects.create()
+        group_1.user.add(teacher)
+        group_2 = Group.objects.create()
+        group_2.user.add(teacher)
+        self.assertEqual((Group.objects.filter(user = teacher).count()) > 1, True)
+    
+    def test_8(self):
+        student = Student.objects.create(username = 'Din')
+        group_1 = Group.objects.create()
+        group_1.user.add(student)
+        group_2 = Group.objects.create()
+        group_2.user.add(student)
+        self.assertEqual((Group.objects.filter(user = student).count()) == 1, True)
+
+'''
     
